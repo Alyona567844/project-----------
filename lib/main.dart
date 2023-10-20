1a07877c53d5179a37de75f396266be0ab663b3e
Alyona567844 <alyona.busova@yandex.ru>
2023-10-20 20:09:06 +0500

    themecolor

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: Colors.amber),
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       getPages: [
